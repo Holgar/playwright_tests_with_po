70d36729272e290301680afcbb17a6c02da0f359
Michael <djibril536@gmail.com>
2023-10-23 20:19:51 +0300

    remove .only

--- src/tests/shopCart.spec.js
+++ src/tests/shopCart.spec.js
@@ -7,7 +7,7 @@ test.describe("Check cart page", () => {
       await loginPage.performLogin("standard_user", "secret_sauce");
     });
 
-    test.only("Add random items to cart", async ({ inventoryPage, shopingCartPage}) => {
+    test("Add random items to cart", async ({ inventoryPage, shopingCartPage}) => {
       const addedItems = await inventoryPage.addRandomItemsToCart();
       await inventoryPage.shopingCart.click();
       const cartItems = await shopingCartPage.getItemsFromCart();
